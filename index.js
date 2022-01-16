6aa8ef84a613101829f320f8edee16f6d74304b4
Daniel Riggs <daniel.riggs1@gmail.com>
2022-01-16 18:28:41 -0500

    This will work!

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const nunjucks = require("nunjucks");
+const fs = require("fs");
 
 const navPages = [
     {name: "page1", human: "Page 1"},
 ];
 
-const s = nunjucks.render("templates/infoWindow.njk", { title: "Jameson", navPages, nextPage: "nextPage", nextPageHuman: "Next Page" });
-console.log(s);
+const h = nunjucks.render(
+    "templates/infoWindow.njk",
+    { title: "Jameson", navPages, nextPage: "nextPage", nextPageHuman: "Next Page" },
+);
+
+fs.writeFileSync("html/infoWindow.html", h);
