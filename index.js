1c1ef049fe552400a3065eb68a9a136b6bb7be62
Daniel Riggs <daniel.riggs1@gmail.com>
2022-01-16 19:05:38 -0500

    Get full loop going

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const nunjucks = require("nunjucks");
 const fs = require("fs");
 
-const navPages = [
-    {name: "page1", human: "Page 1"},
-];
+const pageData = [
+    // ["default", "Home", "Praat scripting - Intro"],
+    ["infoWindow", "Info window", "Praat scripting - Info Window"],
+    /* ["textEditor", "Text editor", "Praat scripting - Text Editor"],
+    ["numVariables", "Numeric variables", "Praat scripting - Numeric variables"],
+    ["stringsIntro", "String variables", "Praat scripting - String variables"],
+    ["strings2", "Strings: continued", "Praat scripting - Strings continued"],
+    ["boolean", "Boolean variables", "Praat scripting - Boolean variables"],
+    ["findingCommands", "Praat commands", "Praat scripting - Finding commands"],
+    ["conditionals", "Conditionals", "Praat scripting - Conditionals"],
+    ["objectSelection", "Object selection", "Praat scripting - Object selection"],
+    ["loops", "Loops", "Praat scripting - Loops"],
+    ["filePaths", "File paths", "Praat scripting - File paths"],
+    ["files", "Files", "Praat scripting - Files"],
+    ["loopingThroughFiles", "Looping through files", "Praat scripting - Looping through files"],
+    ["filesExtendedExample", "Challenge: Formant script", "Praat scripting - Formant script"],
+    ["popupWindows", "Popup windows", "Praat Scripting - Forms, beginPause, Popup windows"],
+    ["procedures", "Procedures", "Praat scripting - Procedures"],
+    ["advancedStrings", "Advanced strings", "Praat scripting - Advanced String Operations"],
+    ["theEnd", "The End", "Praat scripting - The End"],
+    ["downloads", "Downloads", "Praat scripting - Downloads page"],
+    ["contact", "Contact", "Praat scripting - Contact"],
+     */
+].map(([name, humanName, title]) => ({ name, humanName, title }));
 
-const h = nunjucks.render(
-    "templates/infoWindow.njk",
-    { title: "Jameson", navPages, nextPage: "nextPage", nextPageHuman: "Next Page" },
-);
+const numPages = pageData.length;
 
-fs.writeFileSync("html/infoWindow.html", h);
+for (let i = 0; i < numPages; i++) {
+    const page = pageData[i];
+    const nextPage = i === numPages - 1 ? {} : pageData[i + 1];
+    const { name, title } = page;
+    const html = nunjucks.render(
+        `templates/${name}.njk`,
+        { title, pageData, nextPage },
+    );
+    fs.writeFileSync(`html/${name}.html`, html);
+}
+
